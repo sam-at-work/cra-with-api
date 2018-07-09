3a90b2b299a9ad24b5c59339ac499625af7c18f7
Sam Grace <sam@festicket.com>
2018-07-09 18:23:23 +0200

    :art: Improving structure of build scripts

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const PORT = 3003;
 const app = express();
 
-// app.use(express.static('static'));
+app.use(express.static('static'));
 
 app.get("/api", (req, res) => res.json({ message: "Hello from Express API" }));
 app.listen(PORT, () => {
