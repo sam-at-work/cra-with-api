db797ce8839c73f14d8d23bfabe433764b0e3cd1
Sam Grace <sam@festicket.com>
2018-07-10 08:53:57 +0200

    :children_crossing: Run production version on different port

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-const PORT = 3003;
+const PORT = process.env.NODE_ENV === 'production' ? 4004 : 3003;
 const app = express();
 
 app.use(express.static('static'));
@@ -8,4 +8,5 @@ app.use(express.static('static'));
 app.get("/api", (req, res) => res.json({ message: "Hello from Express" }));
 app.listen(PORT, () => {
   console.log(`Node server listening on port ${PORT}!`);
+  console.log(`NODE_ENV is: ${process.env.NODE_ENV}`);
 });
