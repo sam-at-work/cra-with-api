4f57a8d3381deb5a8cc5f6b0ffb7863993e55c42
Sam Grace <sam@festicket.com>
2018-07-15 00:10:27 +0100

    :art: Improve gitmojis script to also update list

--- scripts/gitmoji-config.sh
+++ scripts/gitmoji-config.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# call the gitmoji hook with npx so gitmoji can be local dev dependency
+# Update gitmojis list
+npx gitmoji -u
 
+# call the gitmoji hook with npx so gitmoji can be local dev dependency
 npx gitmoji -i # setup gitmoji commit hook
 sed -Ei '' "s|(gitmoji --hook $1)|npx \1|g" .git/hooks/prepare-commit-msg
