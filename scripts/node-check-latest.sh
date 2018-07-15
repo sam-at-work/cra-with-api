baa870753aa79744437621addec5ba74ce2898e8
Sam Grace <sam@festicket.com>
2018-07-15 22:37:58 +0100

    :sparkles: Add script to check latest version of node

---
+++ scripts/node-check-latest.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e # exit script on error
+
+project_major_version=`node -p "require('./package.json').engines.node"`
+npx check-node-version --node ${project_major_version}
+
+latest_node=`curl https://nodejs.org/en/download/current/ | tr '\n' ' ' | grep -o '<strong>.*</strong>' | sed -E s%\<\/?strong\>%%g`
+npx check-node-version --node ${latest_node}
+
+echo "Node is up to date - `node -v`"
