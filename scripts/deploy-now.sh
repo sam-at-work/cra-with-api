490ff3e1deea78289abce459aa1a5c6847962c34
Sam Grace <sam@festicket.com>
2018-07-15 18:08:59 +0100

    :zap: Imrpove now deploy script

---
+++ scripts/deploy-now.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+app_name=`node -p "require('./package.json').name"`
+
+# Deploy
+echo "Deploying ${app_name}"
+now deploy server/ -e NODE_ENV=production --public --name=${app_name}
+
+# Update url in README.md
+echo "Updating URL in readme for ${app_name}"
+url=`now ls ${app_name} | grep ${app_name} | sed 's/^ *//' | sed -E 's/[[:space:]]+/,/g' | cut -d "," -f 2`
+url=`echo ${url} | sed -E "s,\[[0-9]+m,,g"` # HACK for WebStorm. removes an escape char https://unix.stackexchange.com/a/172001
+
+sed -Ei '' "s|(\[Live Demo\])\(.*\)|\1\($url\)|g" README.md
--- scripts/update-demo-url.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-url=$(cat | tee /dev/tty | grep -o https://.*\.now\.sh)
-sed -Ei '' "s|(\[Live Demo\])\(.*\)|\1\($url\)|g" README.md
