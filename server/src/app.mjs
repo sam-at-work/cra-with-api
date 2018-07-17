46ea17374d7ea3fc2ca0a368a4c7a44c85472323
Sam Grace <sam@festicket.com>
2018-07-17 22:22:21 +0100

    :art: Update babel options
    Use polyfills (found a bug), Don't transform modules, Show debug output

--- server/src/app.mjs
+++ server/src/app.mjs
@@ -1,3 +1,9 @@
+/**
+ * When we use [].values(), a babel bug means it doesn't add the
+ * required polyfill, even with "useBuiltIns": "usage" set in .babelrc.
+ * The import below is a manual work around until the bug is fixed.
+ */
+import "core-js/modules/es6.array.iterator";
 import express from "express";
 
 const isProd = process.env.NODE_ENV === "production";
@@ -10,6 +16,10 @@ if (isProd) app.use(express.static("client-production-build"));
 app.get("/api", (req, res) => res.json({ message: "Hello from Express" }));
 
 app.listen(PORT, () => {
-  console.log(`Node server listening on port ${PORT}!`);
-  console.log(`NODE_ENV is: ${process.env.NODE_ENV}`);
+  for (const m of [
+    `Node server listening on port ${PORT}!`,
+    `NODE_ENV is: ${process.env.NODE_ENV}`
+  ].values()) {
+    console.log(m);
+  }
 });
