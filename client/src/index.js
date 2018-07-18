ea89cc8c314e0baa167a512a019139a7fa68a0f9
Sam Grace <sam@festicket.com>
2018-07-18 20:02:02 +0100

    :heavy_plus_sign: Add react-router with Basic Example

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import BasicExample from "./BasicExample";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<BasicExample />, document.getElementById("root"));
 registerServiceWorker();
