6b0a795e8f019a4f257b5f6ab5bf3cc18e64e789
Sam Grace <sam@festicket.com>
2018-07-31 22:46:20 +0100

    :sparkles: Serve react-router project from node server
    Enables router urls to load correctly when loaded for first time

--- server/src/app.mjs
+++ server/src/app.mjs
@@ -1,11 +1,18 @@
+import path from "path";
 import express from "express";
 
 const isProd = process.env.NODE_ENV === "production";
 const PORT = isProd ? 4004 : 3003;
+const buildDir = path.join(__dirname, '..', "client-production-build");
 
 const app = express();
 
-if (isProd) app.use(express.static("client-production-build"));
+if (isProd) {
+  app.use(express.static(buildDir));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(buildDir, 'index.html'));
+  });
+}
 
 app.get("/api", (req, res) => res.json({ message: "Hello from Express" }));
 
