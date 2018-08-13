b2ba3892bf6afd7ab9b8c571a26a394671c7bb80
Sam Grace <sam@festicket.com>
2018-08-13 14:27:23 +0100

    :sparkles: Update server kill script to kill any port

--- scripts/dev-kill-server.sh
+++ scripts/dev-kill-server.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-lsof -i:3003 | awk 'NR!=1 {print $2}' | xargs kill
+if [ -z "$1" ] ; then
+  port=3003
+else
+  port=$1
+fi
+
+echo "Killing process running on port ${port}"
+lsof -i:$port | awk 'NR!=1 {print $2}' | xargs kill
